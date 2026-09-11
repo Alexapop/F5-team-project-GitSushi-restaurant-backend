d7e3c96f3154743a1f13aaa8c8ac4aa992ab6c2d
Raana <raanaafkari96@gmail.com>
2026-09-11 13:53:31 +0200

    feat: add email lookup methods to UsersRepository

--- src/main/java/dev/team1/users/UsersRepository.java
+++ src/main/java/dev/team1/users/UsersRepository.java
@@ -2,6 +2,12 @@ package dev.team1.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 
 }
