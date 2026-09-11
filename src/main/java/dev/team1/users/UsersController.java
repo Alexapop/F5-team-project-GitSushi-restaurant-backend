559a4f950bf1767157a085d216a806b1c67a10db
Raana <raanaafkari96@gmail.com>
2026-09-11 14:09:29 +0200

    feat: add UsersController with POST /usuarios endpoint

--- src/main/java/dev/team1/users/UsersController.java
+++ src/main/java/dev/team1/users/UsersController.java
@@ -1,5 +1,24 @@
 package dev.team1.users;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsersController {
+
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @PostMapping("/usuarios")
+    public ResponseEntity<UserEntity> createUser(@RequestBody UserEntity newUser) {
+        UserEntity savedUser = usersService.registerUser(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
 
 }
