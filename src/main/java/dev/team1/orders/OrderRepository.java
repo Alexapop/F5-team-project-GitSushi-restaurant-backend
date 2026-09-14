bd8c21c121286c78effc21ae365f1fccc3fbb6b1
Ioana <ioana3popescu@gmail.com>
2026-09-14 11:50:21 +0200

    feat(GS-359): find orders by status

--- src/main/java/dev/team1/orders/OrderRepository.java
+++ src/main/java/dev/team1/orders/OrderRepository.java
@@ -1,9 +1,12 @@
 package dev.team1.orders;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import dev.team1.enums.OrderStatus;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
 
-    // Inherited CRUD methods are sufficient for creating orders for now
+   List <OrderEntity> findByStatus(OrderStatus status);
 
 }
