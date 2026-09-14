e6103f4e5e1681a143802f9f53d39249b6d84315
Ioana <ioana3popescu@gmail.com>
2026-09-14 11:50:14 +0200

    feat(GS-359): add order status

--- src/main/java/dev/team1/orders/OrderEntity.java
+++ src/main/java/dev/team1/orders/OrderEntity.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import dev.team1.enums.OrderStatus;
 import dev.team1.orders_products.OrderProductEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -12,6 +13,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -47,6 +50,10 @@ public class OrderEntity {
     private BigDecimal total;
 
     private String chefNote;
+    
+    @Enumerated(EnumType.STRING)
+    @Column (nullable = false)
+    private OrderStatus status;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderProductEntity> orderProducts = new ArrayList<>();
